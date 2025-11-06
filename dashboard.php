b68d53d7a3a581489c0c8a5e09261b9fa93b33a2
zuhraazzahra658 <zuhraazzahra658@gmail.com>
2025-11-06 11:35:46 +0700

    commit 5

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+// Pastikan user sudah login
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+// Commit 5 – Setup Awal
+echo "<h2>--POLGAN MART--</h2>";
+echo "<p>Selamat datang, " . $_SESSION['username'] . "!</p><hr>";
+
+// Data produk (array)
+$kode_barang = ["B001", "B002", "B003", "B004", "B005"];
+$nama_barang = ["Sabun", "Sampo", "Pasta Gigi", "Tisu", "Detergen"];
+$harga_barang = [5000, 12000, 8000, 7000, 15000];
