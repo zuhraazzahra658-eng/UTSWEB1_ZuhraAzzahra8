60336c026a963226770935be5eb2b200706c49b9
zuhraazzahra658 <zuhraazzahra658@gmail.com>
2025-11-06 11:37:13 +0700

    commit 6

--- dashboard.php
+++ dashboard.php
@@ -15,3 +15,22 @@ echo "<p>Selamat datang, " . $_SESSION['username'] . "!</p><hr>";
 $kode_barang = ["B001", "B002", "B003", "B004", "B005"];
 $nama_barang = ["Sabun", "Sampo", "Pasta Gigi", "Tisu", "Detergen"];
 $harga_barang = [5000, 12000, 8000, 7000, 15000];
+
+
+// Commit 6 – Logika Penjualan Random
+$beli = [];
+$jumlah = [];
+$total = [];
+
+for ($i = 0; $i < count($kode_barang); $i++) {
+    // Random apakah barang dibeli (0 atau 1)
+    $beli[$i] = rand(0, 1);
+    if ($beli[$i] == 1) {
+        $jumlah[$i] = rand(1, 5); // jumlah acak 1–5
+        $total[$i] = $harga_barang[$i] * $jumlah[$i];
+    } else {
+        $jumlah[$i] = 0;
+        $total[$i] = 0;
+    }
+}
+
